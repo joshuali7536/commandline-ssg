8ad01c153a2f41521ce108749b0d001e9a45e6fc
Joshua Li <49960519+joshuali7536@users.noreply.github.com>
2021-10-06 16:43:53 -0400

    update fileFolder/index.js for tests

--- src/fileFolder/index.js
+++ src/fileFolder/index.js
@@ -15,54 +15,67 @@ function fileFolder(options) {
         console.log("Entered config");
         //config.readConfig(options.config);
 
-        if (fs.existsSync(options.config)){
+        if (fs.existsSync(options.config)) {
             var config = JSON.parse(fs.readFileSync(options.config));
-            console.log(config.input);
-            console.log(config.lang);
-            console.log(config.output);
-            console.log(config.stylesheet);
+
+            if (config.input) {
+                console.log(config.input);
+                options.input = config.input;
+            }
+            if (config.output) {
+                console.log(config.output);
+                options.output = config.output;
+            }
+            if (config.lang) {
+                console.log(config.lang);
+                options.lang = config.lang;
+            }
+            if (config.stylesheet) {
+                console.log(config.stylesheet);
+                options.stylesheet = config.stylesheet;
+            }
+
         }
         else {
             console.error("no such config file");
             process.exit(1);
         }
 
-        
+
     }
-    else {
-        folder.createDestination(options.output);
 
-        if (fs.existsSync(options.input)) {
+    folder.createDestination(options.output);
 
-            const stats = fs.statSync(options.input);
-            if (stats.isFile()) {
-                options.input = process.cwd() + '\\' + options.input;
-                file.parseFile(options);
-            }
-            else {
+    if (fs.existsSync(options.input)) {
 
-                const indexFilePath = path.join(process.cwd(), options.output, 'index.html');
-                const toReturn = `<!doctype html><html lang="${options.lang}"><head><meta charset="utf-8"><title>Index file</title><link rel="stylesheet" href="${options.stylesheet}"><meta name="viewport" content="width=device-width, initial-scale=1"></head><body>`;
-
-                fs.writeFileSync(indexFilePath, toReturn, function (err) {
-                    if (err) throw err;
-                    console.log("HTML file created!")
-                });
-                folder.loopThroughAllFiles(options);
-
-                const endingReturn = `</body></html>`;
-                fs.appendFileSync(indexFilePath, endingReturn, function (err) {
-                    if (err) throw err;
-                    console.log("HTML file successfullly completed");
-                })
-
-            }
-
+        const stats = fs.statSync(options.input);
+        if (stats.isFile()) {
+            options.input = process.cwd() + '\\' + options.input;
+            file.parseFile(options);
         }
         else {
-            console.error("no such file/folder");
-            process.exit(1);
+
+            const indexFilePath = path.join(process.cwd(), options.output, 'index.html');
+            const toReturn = `<!doctype html><html lang="${options.lang}"><head><meta charset="utf-8"><title>Index file</title><link rel="stylesheet" href="${options.stylesheet}"><meta name="viewport" content="width=device-width, initial-scale=1"></head><body>`;
+
+            fs.writeFileSync(indexFilePath, toReturn, function (err) {
+                if (err) throw err;
+                console.log("HTML file created!")
+            });
+            folder.loopThroughAllFiles(options);
+
+            const endingReturn = `</body></html>`;
+            fs.appendFileSync(indexFilePath, endingReturn, function (err) {
+                if (err) throw err;
+                console.log("HTML file successfullly completed");
+            })
+
         }
+
+    }
+    else {
+        console.error("no such file/folder");
+        process.exit(1);
     }
 }
 
